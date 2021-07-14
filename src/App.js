70adc86c763caf9be2d8225eb5075a3fddbb0ece
oomuns <86105163+oomuns@users.noreply.github.com>
2021-07-14 14:36:41 +0900

    07.14Study checkpoint

--- src/App.js
+++ src/App.js
@@ -3,33 +3,34 @@ import React, { Component } from "react";
 import Subject from "./components/Subject"; //Subject load
 import TOC from "./components/TOC";
 import Content from "./components/Content";
+import Control from "./components/Control";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/Createcontent";
+
 
 class App extends Component {
     constructor(props) {
         //consructor 가 제일 먼저 실행 되면서 초기화를 담당한다.
         super(props);
         this.state = {
-            mode: "welcome",
+            mode: "create",
             selected_content_id: 2,
             subject: { title: "WEB2", sub: "World Wid Web!" },
             welcome: { title: "Welcome", desc: "Hello React!" },
             contents: [
                 { id: 1, title: "HTML", desc: "Html is Hypertext..." },
                 { id: 2, title: "CSS", desc: "CSS is for design" },
-                {
-                    id: 3,
-                    title: "Javascript",
-                    desc: "Javascript is for interactive"
-                }
+                {id: 3, title: "Javascript", desc: "Javascript is for interactive"}
             ]
         };
     }
     render() {
         var _title,
-            _desc = null;
+            _desc, _article = null;
         if (this.state.mode === "welcome") {
             _title = this.state.welcome.title;
             _desc = this.state.welcome.desc;
+            _article = <ReadContent title = {_title} desc = {_desc}></ReadContent>
         } else if (this.state.mode === "read") {
             var i = 0;
             while (i < this.state.contents.length) {
@@ -41,6 +42,17 @@ class App extends Component {
                 }
                 i = i + 1;
             }
+            _article = <ReadContent title = {_title} desc = {_desc}></ReadContent>
+        } else if(this.state.mode === 'create'){
+            _article = <CreateContent onSubmit = {function(_title,_desc){
+                this.max_content_id = this.max_content_d + 1;
+                var _contents = this.state.contents.concat(
+                    {id : this.max_content_id, title : _title, desc : _desc}
+                );
+                this.setState({
+                    contents : _contents
+                });
+            }.bind(this)}></CreateContent>
         }
         return (
             <div className="App">
@@ -61,6 +73,12 @@ class App extends Component {
                     }.bind(this)}
                     data={this.state.contents}
                 ></TOC>
+                <Control onChangeMode = {function(mode){
+                    this.setState({
+                        mode : mode
+                    });
+                }.bind(this)}></Control>
+                {_article}
                 <Content title={_title} desc={_desc}></Content>
             </div>
         );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 
 class TOC extends Component {
+    shouldComonentUpdate(newProps, newState){   //render이전에 호출된다, false면 render가 호출되지 않도록 약속되어있다.
+        console.log(newProps.data, this.props.data);
+        if(this.props.data === newProps.data){
+            return false;
+        }
+        return true;
+    }
     render() {
+        console.log("TOCRENDER");
         var lists = [];
         var data = this.props.data;
         var i = 0;
@@ -13,7 +21,7 @@ class TOC extends Component {
                         data-id={data[i].id}
                         onClick={function (e) {
                             e.preventDefault();
-                            this.props.onChangePage(e.target.dataset.id);
+                            this.props.onChangePage(e.target.dataset.id);   //props는 readonly
                         }.bind(this)}
                     >
                         {data[i].title}
