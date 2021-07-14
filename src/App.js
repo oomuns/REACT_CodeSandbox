e7a1a90f3893e1ff453b04d896fb61d3fc159b42
oomuns <86105163+oomuns@users.noreply.github.com>
2021-07-14 11:01:52 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./styles.css";
+import React, { Component } from "react";
+import Subject from "./components/Subject"; //Subject load
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+
+class App extends Component {
+    constructor(props) {
+        //consructor 가 제일 먼저 실행 되면서 초기화를 담당한다.
+        super(props);
+        this.state = {
+            mode: "welcome",
+            selected_content_id: 2,
+            subject: { title: "WEB2", sub: "World Wid Web!" },
+            welcome: { title: "Welcome", desc: "Hello React!" },
+            contents: [
+                { id: 1, title: "HTML", desc: "Html is Hypertext..." },
+                { id: 2, title: "CSS", desc: "CSS is for design" },
+                {
+                    id: 3,
+                    title: "Javascript",
+                    desc: "Javascript is for interactive"
+                }
+            ]
+        };
+    }
+    render() {
+        var _title,
+            _desc = null;
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode === "read") {
+            var i = 0;
+            while (i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if (data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+                    break;
+                }
+                i = i + 1;
+            }
+        }
+        return (
+            <div className="App">
+                <h1>Hello world! </h1>
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}
+                    onChangePage={function () {
+                        this.setState({ mode: "welcome" });
+                    }.bind(this)}
+                ></Subject>
+                <TOC
+                    onChangePage={function (id) {
+                        this.setState({
+                            mode: "read",
+                            selected_content_id: Number(id)
+                        });
+                    }.bind(this)}
+                    data={this.state.contents}
+                ></TOC>
+                <Content title={_title} desc={_desc}></Content>
+            </div>
+        );
+    }
+}
+
+export default App;
